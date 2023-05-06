8a1684d465bfa03ad9e6762f46fe3f3386b5bfc1
Anthony Christoforou <christoforouanthony@gmail.com>
2023-05-06 20:33:46 +0200

    First try at modelising data

--- wind_power_forecasting/data.py
+++ wind_power_forecasting/data.py
@@ -1,11 +1,98 @@
-import torch
+#!/usr/bin/env python
+""" Custom dataset class.
 
-class Dataset(torch.utils.data.Dataset):
-    def __init__(self):
-        pass
+This module contains the custom dataset class that will be used to load the
+wind power production data for the different models.
+"""
+
+__authors__ = ["Anthony Christoforou", "Ethan Arm"]
+__contact__ = ["anthony.christoforou@etu.unige.ch", "ethan.arm@etu.unige.ch"]
+__copyright__ = "Copyright 2023, Unige"
+__credits__ = ["Anthony Christoforou", "Ethan Arm"]
+__date__ = "2023/05/06"
+__deprecated__ = False
+__license__ = "GPLv3"
+__maintainer__ = "Anthony Christoforou"
+__status__ = "Development"
+__version__ = "0.0.1"
+
+# Libraries
+import torch
+import pandas as pd
+
+class WindDataset(torch.utils.data.Dataset):
+    """A class to load the wind power production data.
+
+    Attributes
+    ----------
+    data : pandas.DataFrame
+        The data loaded from the data file.
+    relative_positions : pandas.DataFrame
+        The relative positions loaded from the relative position file.
+    transform : callable, optional
+        A function/transform that takes in a sample and returns a transformed
+        version.
+    target_transform : callable, optional
+        A function/transform that takes in the target and transforms it.
+
+    Methods
+    -------
+    __len__()
+        Returns the length of the dataset.
+    __getitem__(index)
+        Returns one sample of the dataset.
+    """
+    def __init__(self, data_dir, relative_position_file, transform=None, target_transform=None):
+        """Constructs all the necessary attributes for the Dataset object.
+
+        Parameters
+        ----------
+        data_dir : str
+            The path to the data file.
+        relative_position_file : str
+            The path to the relative position file.
+        transform : callable, optional
+            A function/transform that takes in a sample and returns a transformed
+            version.
+        target_transform : callable, optional
+            A function/transform that takes in the target and transforms it.
+        """
+        self.data = pd.read_csv(data_dir)
+        self.relative_positions = pd.read_csv(relative_position_file)
+        self.transform = transform
+        self.target_transform = target_transform
     
     def __len__(self):
-        pass
+        """Returns the number of samples.
+        """
+        return len(self.data)
     
     def __getitem__(self, index):
-        pass
+        """Returns the sample of the dataset at the given index.
+
+        Parameters
+        ----------
+        index : int
+            The index of the sample to return.
+            
+        Returns
+        -------
+        features : pandas.Series
+            The features of the sample.
+        labels : pandas.Series
+            The label of the sample. (The wind power production)
+        relative_position : pandas.Series
+            The relative position of the sample.
+        
+        Notes
+        """
+        labels = self.data.iloc[index, -1]
+        features = self.data.iloc[index, :-1]
+        relative_position = self.relative_positions.iloc[index, :]
+        
+        if self.transform:
+            features = self.transform(features)
+        if self.target_transform:
+            labels = self.target_transform(labels)
+            
+        return features, labels, relative_position
--- wind_power_forecasting/main.py
+++ wind_power_forecasting/main.py
@@ -1,5 +1,82 @@
+#!/usr/bin/env python
+""" Main module of the project.
+
+This module is the main module of the project. It contains the main function
+that is executed when the program is run. This will call and compare different
+models to predict the wind power production of a wind farm.
+"""
+
+__authors__ = ["Anthony Christoforou", "Ethan Arm"]
+__contact__ = ["anthony.christoforou@etu.unige.ch", "ethan.arm@etu.unige.ch"]
+__copyright__ = "Copyright 2023, Unige"
+__credits__ = ["Anthony Christoforou", "Ethan Arm"]
+__date__ = "2023/05/06"
+__deprecated__ = False
+__license__ = "GPLv3"
+__maintainer__ = "Anthony Christoforou"
+__status__ = "Development"
+__version__ = "0.0.1"
+
+# Libraries
+import os
+import torch
+from torch.utils.data import DataLoader
+import pandas as pd
+import urllib.request
+
+# Modules
+from .data import WindDataset
+from .utils.progressbar import ProgressBar
+
+# Functions
+def download_data(url, filename):
+    """Download datasets from the internet if they are not already present.
+    """
+    
+    data_dir = "./wind_power_forecasting/data/"
+    
+    if not os.path.exists(filename):
+        if not os.path.exists(os.path.dirname(data_dir)):
+            os.makedirs(os.path.dirname(data_dir))
+        print(f"Downloading {filename}...")
+        urllib.request.urlretrieve(url, filename, ProgressBar())
+        print(f"{filename} downloaded.")
+    else:
+        print(f"{filename} already exists.")
+
 def main():
-    print("Hello World!")
+    """ Main function of the project.
+    """
+    
+    # Download the data if it is not already present
+    data_url = "https://bj.bcebos.com/v1/ai-studio-online/85b5cb4eea5a4f259766f42a448e2c04a7499c43e1ae4cc28fbdee8e087e2385?responseContentDisposition=attachment%3B%20filename%3Dwtbdata_245days.csv&authorization=bce-auth-v1%2F0ef6765c1e494918bc0d4c3ca3e5c6d1%2F2022-05-05T14%3A17%3A03Z%2F-1%2F%2F5932bfb6aa3af1bcfb467bf2a4a6877f8823fe96c6f4fd0d4a3caa722354e3ac"
+    relative_position_url = "https://bj.bcebos.com/v1/ai-studio-online/e927ce742c884955bf2a667929d36b2ef41c572cd6e245fa86257ecc2f7be7bc?responseContentDisposition=attachment%3B%20filename%3Dsdwpf_baidukddcup2022_turb_location.CSV&authorization=bce-auth-v1%2F0ef6765c1e494918bc0d4c3ca3e5c6d1%2F2022-04-11T08%3A27%3A09Z%2F-1%2F%2Fcf377452dbd186873680f2f0fe39200b3de86083a036da220ab8a02abc5a8032"
+    
+    data_dir = "./wind_power_forecasting/data/wtbdata_245days.csv"
+    relative_position_file = "./wind_power_forecasting/data/sdwpf_baidukddcup2022_turb_location.CSV"
+    
+    download_data(data_url, data_dir)
+    download_data(relative_position_url, relative_position_file)
+    
+    # Check if GPU is available
+    device = (
+        "cuda"
+        if torch.cuda.is_available()
+        else "mps"
+        if torch.backends.mps.is_available()
+        else "cpu"
+    )
+    
+    # Load the dataset
+    dataset = WindDataset(data_dir, relative_position_file)
+    # We take the first 100 samples just to test the code
+    dataset = dataset[:100]
+    
+    dataloader = DataLoader(dataset, batch_size=20, shuffle=False)
+    
+    print(dataset)
+    print(f"Dataset length: {len(dataset)}")
+    
     
 if __name__ == "__main__":
     main()
