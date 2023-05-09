271243b516ffbe5d9cd25569a0b7e2fe4243cbe1
Anthony Christoforou <christoforouanthony@gmail.com>
2023-05-09 11:48:58 +0200

    renamed to neural_network

---
+++ wind_power_forecasting/neural_network/model.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+""" Neural network module.
+
+This module contains the neural network class that is used to predict the wind
+power production of a wind farm.
+"""
+
+__authors__ = ["Anthony Christoforou", "Ethan Arm"]
+__contact__ = ["anthony.christoforou@etu.unige.ch", "ethan.arm@etu.unige.ch"]
+__copyright__ = "Copyright 2023, Unige"
+__credits__ = ["Anthony Christoforou", "Ethan Arm"]
+__date__ = "2023/05/06"
+__deprecated__ = False
+__license__ = "GPLv3"
+__maintainer__ = "Anthony Christoforou"
+__status__ = "Development"
+__version__ = "0.0.1"
+
+# Libraries
+import os
+import torch
+from torch import nn
+import pandas as pd
+
+class NeuralNetwork(nn.Module):
+    pass
